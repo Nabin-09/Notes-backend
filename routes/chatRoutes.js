0a7d45894d0bb17810c70f605f534682c3b7fe80
Nabin <sharmanabin.0910@gmail.com>
2025-12-07 00:33:14 +0530

    removal of AI agents

--- routes/chatRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-import { chatWithAgent } from '../controllers/chatController.js'
-
-const router = express.Router();
-router.post('/' , chatWithAgent)
-
-export default router
